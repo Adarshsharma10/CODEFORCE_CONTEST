343eada2c8608be46930f2dfecb22d57282fb2b7
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-01-19 13:21:47 +0530

    div 4

---
+++ Codeforces Round 1074 (Div. 4)/A_Perfect_Root.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+
+void solve() {
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cout<<i+1<<" ";
+    }cout<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+// Successfully Submitted
