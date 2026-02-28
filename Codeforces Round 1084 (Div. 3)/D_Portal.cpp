e7ebf5774c4095e25275c8074416d936637a67dc
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-02-28 12:16:07 +0530

    good implentation

--- Codeforces Round 1084 (Div. 3)/D_Portal.cpp
+++ Codeforces Round 1084 (Div. 3)/D_Portal.cpp
@@ -1,51 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define ll long long
-#define all(v) (v).begin(), (v).end()
+void rotateArr(vector<int>& arr, int d) {
+    int n = arr.size();
+    if (n == 0) return;
+    d %= n;
+    reverse(arr.begin(), arr.begin() + d);
+    reverse(arr.begin() + d, arr.end());
+    reverse(arr.begin(), arr.end());
+}
 
 void solve() {
-    int n,x,y;
-    cin >>n>>x>>y;
-    vector<int> p;
-    for(auto &x : p){
-        cin>>x;
-    }
-    int mini=INT_MAX;
-    int min_idx;
-    for(int i=x;i<y;i++){
-        if(mini>p[i]){
-            mini=p[i];
-            min_idx=i;
-        }
-    }
-    vector<int> store;
-    int m = y-x;
-    for(int i=0;i<m;i++){
-        store.push_back(p[(min_idx+i)%m]);
-    }
-
-    int l_idx = max_element(p.begin(),p.begin()+x-1)-p.begin();
-    int l = *max_element(p.begin(),p.begin()+x-1);
-    deque<int> left(p.begin(),p.begin()+x-1);
-    if(l>mini){
-        for(int i=l_idx;i<x;i++){
-
-        }
-    }
-      
+    int n, x, y;
+    if (!(cin >> n >> x >> y)) return;
     
+    vector<int> v(n);
+    deque<int> dq1, dq3;
+    vector<int> arr;
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+        if (i < x) dq1.push_back(v[i]);
+        else if (i < y) arr.push_back(v[i]);
+        else dq3.push_back(v[i]);
+    }
+
+    if (arr.empty()) {
+        for(int i : dq1) cout << i << " ";
+        for(int i : dq3) cout << i << " ";
+        cout << endl;
+        return;
+    }
+    int minele = *min_element(arr.begin(), arr.end());
+    int ind = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == minele) {
+            ind = i;
+            break; 
+        }
+    }
+    
+    rotateArr(arr, ind);
+    int arrmin = arr[0];
+
+    bool flag1 = false;
+    deque<int> temp_dq1;
+    while (!dq1.empty()) {
+        if (dq1.front() > arrmin) {
+            flag1 = true;
+            break; 
+        }
+        temp_dq1.push_back(dq1.front());
+        dq1.pop_front();
+    }
+    if (flag1) {
+        while (!dq1.empty()) {
+            dq3.push_front(dq1.back());
+            dq1.pop_back();
+        }
+    }
+    dq1 = temp_dq1;
+    if (!flag1) {
+        while (!dq3.empty() && dq3.front() < arrmin) {
+            dq1.push_back(dq3.front());
+            dq3.pop_front();
+        }
+    }
+    for (int val : dq1) cout << val << " ";
+    for (int val : arr) cout << val << " ";
+    for (int val : dq3) cout << val << " ";
+    cout << endl;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;
     cin >> t;
     while (t--) {
         solve();
     }
-
     return 0;
 }
+
+// Successfully Submitted
